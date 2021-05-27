a1817f67adb77f2d4fd646f0b1f596b9a2d57263
Álvaro Sánchez <forensor@hotmail.com>
2021-05-28 01:08:38 +0200

    ed

---
+++ estante digital/panel.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mostrarMenu = () => {
+    document.getElementById("sidepanel").style.display = "block";
+    document.getElementById("bg").style.display = "block";
+};
+
+const cerrarMenu = () => {
+    document.getElementById("sidepanel").style.display = "none";
+    document.getElementById("bg").style.display = "none";
+}
